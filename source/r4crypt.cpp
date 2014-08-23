84f8ee950e176d44659d823ca0679df66ba60e70
cyrozap <cyrozap@gmail.com>
2014-08-23 12:05:26 -0400

    Added changes to fix building on OS X

--- source/r4crypt.cpp
+++ source/r4crypt.cpp
@@ -17,7 +17,11 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+#ifdef __APPLE__
+#include <stdlib.h>
+#else
 #include <malloc.h>
+#endif
 #include <string.h>
 #include <stdio.h>
 
